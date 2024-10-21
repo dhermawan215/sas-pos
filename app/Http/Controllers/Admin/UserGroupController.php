10e46681cbb74e48b28427c6ada7d5d9f9639bb9
dhermawan215 <dhermawan215@gmail.com>
2024-10-21 23:12:43 +0700

    feat: asset adding to project

---
+++ app/Http/Controllers/Admin/UserGroupController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserGroupController extends Controller
+{
+    protected $sysMenu = 'user_group';
+}
